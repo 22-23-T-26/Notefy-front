d5ede4454bc72ba3ef6b53d112c48473af79098f
MarjanMaci <marjanmarjanovski13@gmail.com>
2023-05-20 18:06:34 +0200

    New Material Modal UI

--- src/components/Modals/NewMaterial.jsx
+++ src/components/Modals/NewMaterial.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 const NewMaterialModal = ({ showModal, handleClose }) => {
   const closeModal = () => {
@@ -10,17 +11,55 @@ const NewMaterialModal = ({ showModal, handleClose }) => {
   return (
     <Modal show={showModal} onHide={closeModal}>
       <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Title>Објави материјали</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant='secondary' onClick={closeModal}>
-          Close
-        </Button>
-        <Button variant='primary' onClick={closeModal}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-2" controlId="formTitle">
+            <Form.Label>Наслов</Form.Label>
+            <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formFaculty">
+            <Form.Label>Факултет</Form.Label>
+            <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formSubject">
+            <Form.Label>Предмет</Form.Label>
+            <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formCategory">
+            <Form.Label>Категорија</Form.Label>
+            <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formDescription">
+            <Form.Label>Опис</Form.Label>
+            <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="text" placeholder="" />
+          </Form.Group>
+
+          <div className='d-flex flex-row justify-content-center gap-5'>
+            <Form.Group className="mb-2" controlId="formPicture" style={{width:'45%'}}>
+              <Form.Label>Слика</Form.Label>
+              <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="file" placeholder="" />
+            </Form.Group>
+            <Form.Group className="mb-2" controlId="formDocument" style={{width:'45%'}}>
+              <Form.Label>Документ</Form.Label>
+              <Form.Control style={{background: "#F4F4F4", borderRadius: "12px", boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25);"}} type="file" placeholder="" />
+            </Form.Group>
+          </div>
+          <div className='d-flex flex-row justify-content-center gap-5'>
+            <Button onClick={closeModal} style={{width:'50%', background: "#F4F4F4", borderRadius: "12px", color: 'black', border: "0"}}>
+              Откажи
+            </Button>
+            <Button onClick={closeModal} type='submit' style={{width:'50%', background: "#D7D7D7", borderRadius: "12px", color: 'black', border: "0"}}>
+              Објави
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 };
