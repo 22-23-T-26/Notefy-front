217e2364ddafdddda5e1a1896c6eaaad949e5ad5
Jeton Ramadani <jeton.ramadani@students.finki.ukim.mk>
2023-05-26 12:48:18 +0200

    handling create a discussion

--- src/components/Modals/DiscussionModal.jsx
+++ src/components/Modals/DiscussionModal.jsx
@@ -1,26 +1,70 @@
 import React from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({
+    title: Yup.string()
+        .required('Title is required'),
+    description: Yup.string()
+        .required('Description is required'),
+});
 function DiscussionModal(props) {
+    const formik = useFormik({
+        initialValues: { title: '', description: '' },
+        validationSchema: validationSchema,
+        onSubmit: values => {
+            props.onAdd(values);
+            formik.resetForm();
+            props.onHide();
+        },
+    });
+    const handleClose = () => {
+        formik.resetForm();
+        props.onHide();
+    }
+
     return (
-        <Modal {...props}>
+        <Modal show={props.show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Започни нова дискусија</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" />
-                    </Form.Group>
-                </Form>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input
+                            id="title"
+                            name="title"
+                            type="text"
+                            className={'form-control' + (formik.errors.title && formik.touched.title ? ' is-invalid' : '')}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.title}
+                        />
+                        {formik.touched.title && formik.errors.title ? (
+                            <div className="invalid-feedback">{formik.errors.title}</div>
+                        ) : null}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            id="description"
+                            name="description"
+                            className={'form-control' + (formik.errors.description && formik.touched.description ? ' is-invalid' : '')}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.description}
+                        />
+                        {formik.touched.description && formik.errors.description ? (
+                            <div className="invalid-feedback">{formik.errors.description}</div>
+                        ) : null}
+                    </div>
+
+                </form>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={props.onHide}>Add</Button>
+                <Button onClick={formik.handleSubmit}>Add</Button>
             </Modal.Footer>
         </Modal>
     );
--- src/services/ForumService.jsx
+++ src/services/ForumService.jsx
@@ -5,9 +5,13 @@ const ForumService = (page) => {
     const getDiscussions = async (page) => {
         return await axiosInstance.get('/api/discussions?size=8&page=' + (page - 1));
     }
+    const createDiscussion = async (discussion) => {
+        return await axiosInstance.post('/api/discussions', discussion);
+    }
 
     return {
-        getDiscussions
+        getDiscussions,
+        createDiscussion
     }
 
 }
