b57cbfc9f17f9a68e7b552cb9d62cdbfc122e6b9
unknown <marjanmarjanovski13@gmail.com>
2023-05-28 15:02:42 +0200

    Small changes

--- src/components/Materials/MaterialsCard.jsx
+++ src/components/Materials/MaterialsCard.jsx
@@ -10,7 +10,7 @@ const MaterialsCard = ({data}) => {
                 <RiFileTextLine className='text-dark' size={60}/>
                 <div className='d-flex flex-column justify-content-left' style={{ marginLeft: '15px'}}>
                     <span className="text-left" style={{fontWeight: '700', fontSize: '16px' }}>{data.title}</span>
-                    <span className="text-left" style={{fontWeight: '500', fontSize: '16px' }}>од {data.createdBy}</span>
+                    <span className="text-left" style={{fontWeight: '500', fontSize: '16px' }}>од {data.createdBy?.username}</span>
                 </div>
             </div>
             <span style={{ marginRight: '30px'}}>
